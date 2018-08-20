99677305b728a549a24f92dcd20b7299d90e6f74
Stuti Chourasia <stuti.chourasia@thomsonreuters.com>
2018-08-20 20:53:47 +0530

    added missing tests

---
+++ E2EAutoJenkins/src/testcase/testscript/Test1.java
@@ -0,0 +1,11 @@
+package testcase.testscript;
+
+
+	import org.testng.annotations.Test;
+
+	public class Test1 {
+		@Test
+		public void addLocationTestCase() {
+			System.out.println("Im in add location test case");
+		}
+	}
---
+++ E2EAutoJenkins/src/testcase/testscript/Test2.java
@@ -0,0 +1,11 @@
+package testcase.testscript;
+
+
+	import org.testng.annotations.Test;
+
+	public class Test2 {
+		@Test
+		public void editLocationTestCase() {
+			System.out.println("Im in edit location test case");
+		}
+	}
